9d50fa6a2106405f05ed7eed2e9d749ef948b541
CrossyX <5840422@student.swin.edu.au>
2018-04-23 23:05:22 +1000

    3rd commit

--- js/spotifyJS.js
+++ js/spotifyJS.js
@@ -31,3 +31,95 @@ $(document).ready(function() {
     	jQuery(element).html(voteNumber);
     })
 });
+
+
+
+$(document).ready(function() {
+	jQuery(".party-category").on('click', function(){
+    	// jQuery(".vote-selector").css('display', 'initial');
+    	// jQuery(".playlist-content:not(.vote-selector)").css('display', 'none');
+
+
+    	jQuery(".playlist-content:not(.vote-selector)").fadeOut(function(){
+    		jQuery(".vote-selector").fadeIn();	
+    	});
+	});
+});
+
+$(document).ready(function() {
+	jQuery(".playlist-category").on('click', function(){
+    	// jQuery(".playlist-selector").css('display', 'initial');
+    	// jQuery(".playlist-content:not(.playlist-selector)").css('display', 'none');
+
+    	jQuery(".playlist-content:not(.playlist-selector)").fadeOut(function(){
+    		jQuery(".playlist-selector").fadeIn();	
+    	});
+	});
+});
+
+$(document).ready(function() {
+	jQuery(".album-category").on('click', function(){
+    	// jQuery(".album-selector").css('display', 'initial');
+    	// jQuery(".playlist-content:not(.album-selector)").css('display', 'none');
+
+    	jQuery(".playlist-content:not(.album-selector)").fadeOut(function(){
+    		jQuery(".album-selector").fadeIn();	
+    	});
+	});
+});
+
+$(document).ready(function() {
+	jQuery(".artist-category").on('click', function(){
+    	// jQuery(".artist-selector").css('display', 'initial');
+    	// jQuery(".playlist-content:not(.artist-selector)").css('display', 'none');
+
+    	jQuery(".playlist-content:not(.artist-selector)").fadeOut(function(){
+    		jQuery(".artist-selector").fadeIn();	
+    	});
+	});
+});
+
+$(document).ready(function() {
+	jQuery(".song-category").on('click', function(){
+    	// jQuery(".song-selector").css('display', 'initial');
+    	// jQuery(".playlist-content:not(.song-selector)").css('display', 'none');
+
+    	jQuery(".playlist-content:not(.song-selector)").fadeOut(function(){
+    		jQuery(".song-selector").fadeIn();	
+    	});
+	});
+});
+
+
+
+/*$(document).ready(function() {
+	jQuery(".party-category").on('click', function(){
+    	jQuery(".main-content").attr('src', 'vote.html');
+	});
+});
+
+$(document).ready(function() {
+	jQuery(".playlist-category").on('click', function(){
+    	jQuery(".main-content").attr('src', 'playlist.html');
+	});
+});
+
+$(document).ready(function() {
+	jQuery(".album-category").on('click', function(){
+    	jQuery(".main-content").attr('src', 'album.html');
+	});
+});
+
+$(document).ready(function() {
+	jQuery(".artist-category").on('click', function(){
+    	jQuery(".main-content").attr('src', 'artist.html');
+	});
+});
+
+$(document).ready(function() {
+	jQuery(".song-category").on('click', function(){
+    	jQuery(".main-content").attr('src', 'songs.html');
+	});
+});*/
+
+
